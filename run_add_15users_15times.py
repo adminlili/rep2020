198cef0fbaf81988fe188067bb732ba8f0367f41
Main_Proger <main_proger@gmail.com>
2020-05-25 09:16:14 +0000

    added line platform details in script run_add_15users...

--- run_add_15users_15times.py
+++ run_add_15users_15times.py
@@ -1,8 +1,17 @@
 #!/usr/bin/pyhon3
 
 import os
+import platform
+
+info = {}
+
+platform_details = platform.platform()
+info["platform details"] = platform_details
+
+
 
 i = 0
 while i < 15:
 	os.system("/root/scripts/add_15users_sh_py/add_15users.sh")
 	i += 1
+
